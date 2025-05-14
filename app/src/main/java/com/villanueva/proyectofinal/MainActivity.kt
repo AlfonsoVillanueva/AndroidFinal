539aa8e06092c189a285efe141d3795cb28bcb29
ReneJosafatCR <149644777+ReneJosafatCR@users.noreply.github.com>
2025-05-13 23:15:03 -0600

    se agrego las bases del programa, un menu que se mantiene siempre activo y intercambio de vistas entre los fragmentos 1 2 y 3

--- app/src/main/java/com/villanueva/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/villanueva/proyectofinal/MainActivity.kt
@@ -1,20 +1,72 @@
 package com.villanueva.proyectofinal
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navegation : BottomNavigationView
+
+    private val mOnNavMenu = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
+        when (item.itemId){
+            R.id.itemFrament1 -> {
+                supportFragmentManager.commit {
+                    replace<PrimerFragment>(R.id.frameContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.itemFrament2 -> {
+                supportFragmentManager.commit {
+                    replace<SegundoFragment>(R.id.frameContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.itemFrament3 -> {
+                supportFragmentManager.commit {
+                    replace<TercerFragment>(R.id.frameContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        navegation = findViewById(R.id.navMenu)
+        navegation.setOnNavigationItemSelectedListener(mOnNavMenu)
+
+        supportFragmentManager.commit {
+            replace<PrimerFragment>(R.id.frameContainer)
+            setReorderingAllowed(true)
+            addToBackStack("replacement")
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
     }
 }
