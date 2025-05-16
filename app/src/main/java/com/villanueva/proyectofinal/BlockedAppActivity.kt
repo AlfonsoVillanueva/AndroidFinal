0c3c0a8f74d1929346cda69b09a6cfa0e11818c3
P3RCI <charlegend403@gmail.com>
2025-05-15 19:43:41 -0600

    Mejoras de la activity de bloqueo
    dame 2 d carne 4 de choclo 5 de carne 1 de choclo 5 de choclo 5 de choclo 2 de choclo 1 de carne otra d choclo 7 d carne 2 de verdura 4 d choclo 1 de atun 3 de carne 4 de choclo

--- app/src/main/java/com/villanueva/proyectofinal/BlockedAppActivity.kt
+++ app/src/main/java/com/villanueva/proyectofinal/BlockedAppActivity.kt
@@ -1,15 +1,37 @@
 package com.villanueva.proyectofinal
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 
 class BlockedAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blocked_app)
 
+        // Mostrar el GIF
+        val imageView = findViewById<ImageView>(R.id.gif2imageView)
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.gato)
+            .into(imageView)
+
+
+        val btnVolver = findViewById<Button>(R.id.closeButton)
+        btnVolver.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+            //finish()
+        }
     }
-
-
 }
+
+
